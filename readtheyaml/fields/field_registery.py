048cb6cba34e69e4f396fccc2596fc9eef182169
therealmarvin <nakedtoothbrush@hotmail.com>
2025-05-11 08:50:18 -0400

    implement enum fields

--- readtheyaml/fields/field_registery.py
+++ readtheyaml/fields/field_registery.py
@@ -1,8 +1,7 @@
-import re
 from functools import partial
-from typing import Optional
 
 from readtheyaml.fields.bool_field import BoolField
+from readtheyaml.fields.enum_field import EnumField
 from readtheyaml.fields.numerical_field import NumericalField
 from readtheyaml.fields.string_field import StringField
 
@@ -11,6 +10,7 @@ FIELD_REGISTRY = {
     "float": partial(NumericalField, float),
     "str": StringField,
     "bool": BoolField,
+    "enum": EnumField,
     # We handle 'list(...)' dynamically
 }
 
