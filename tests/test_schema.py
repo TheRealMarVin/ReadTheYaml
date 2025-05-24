9c5aaaa4c54c6788db2076c1415912ac0c5d9636
therealmarvin <nakedtoothbrush@hotmail.com>
2025-05-23 20:33:10 -0400

    updating unit tests

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -11,10 +11,13 @@ def test_valid_tuple_round_brackets():
     field = _parse_field_type("tuple(int, str)")
     assert field.func.__name__ == "TupleField"
 
-def test_invalid_bracket_mix():
+def test_invalid_tuple_bracket_mix():
     with pytest.raises(ValueError, match="Mismatched brackets"):
         _parse_field_type("tuple[int, str)")
 
-def test_unknown_type():
+    with pytest.raises(ValueError, match="Mismatched brackets"):
+        _parse_field_type("tuple(int, str]")
+
+def test_tuple_unknown_type():
     with pytest.raises(ValueError, match="Unknown field type: foo"):
         _parse_field_type("tuple[foo, str]")
