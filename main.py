7cc86a8b3a981e1e5067d22d544d1dccd04ea0a5
therealmarvin <nakedtoothbrush@hotmail.com>
2025-06-22 18:31:12 -0400

    first version of data_instances

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import argparse
 import sys
 from pathlib import Path
 
+import yaml
+
+from readtheyaml.data_instance import DataInstance
 from readtheyaml.exceptions.validation_error import ValidationError
 from readtheyaml.schema import Schema
 
@@ -16,6 +19,7 @@ def main():
     args = parser.parse_args()
 
     try:
+
         schema = Schema.from_yaml(args.schema, Path("./examples"))
         validated_config = schema.validate_file(args.config, strict=False)
         print("✅ Config is valid!")
@@ -23,7 +27,20 @@ def main():
         print(f"❌ Validation failed: {e}", file=sys.stderr)
         sys.exit(1)
 
-    print(validated_config)
+    try:
+        yaml_path = Path(args.config)
+        with open(yaml_path, "r", encoding="utf-8") as f:
+            yaml_data = yaml.safe_load(f)
+
+        schema = Schema.from_yaml(args.schema, Path("./examples"))
+        data_instance = DataInstance(data=yaml_data, schema=schema, build_now=True)
+        validated_config = schema.validate_file(args.config, strict=False)
+        print("✅ Config is valid!")
+    except ValidationError as e:
+        print(f"❌ Validation failed: {e}", file=sys.stderr)
+        sys.exit(1)
+
+    print(data_instance.dump(file=None))
 
 
 if __name__ == "__main__":
---
+++ readtheyaml/data_instance.py
@@ -0,0 +1,28 @@
+import sys
+
+import yaml
+
+class DataInstance:
+    def __init__(self, data: dict, schema, build_now=True):
+        self.schema = schema
+        self.raw = data
+        self.built = None
+
+        tata = self.schema.build_and_validate(self.raw, strict=False)
+
+        # if build_now:
+        #     self.built = self._build()
+
+    def _build(self):
+        result = {}
+        for key, field in self.schema.fields.items():
+            result[key] = field.build(self.raw.get(key))
+        return result
+
+    def __getitem__(self, key):
+        if self.built is None:
+            self.built = self._build()
+        return self.built[key]
+
+    def dump(self, file=None):
+        yaml.safe_dump(self.raw, file or sys.stdout)
